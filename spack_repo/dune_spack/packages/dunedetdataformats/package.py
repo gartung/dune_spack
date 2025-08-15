908bb33e20e2a2f9641514d7d17b8fe1b2e26396
Patrick Gartung <gartung@fnal.gov>
2025-08-15 13:02:22 -0500

    Update dunedetdataformats

--- spack_repo/dune_spack/packages/dunedetdataformats/package.py
+++ spack_repo/dune_spack/packages/dunedetdataformats/package.py
@@ -11,6 +11,7 @@ class Dunedetdataformats(Package):
     """Dunedetdataformats"""
     git = "https://github.com/DUNE/dunedetdataformats.git"
     url = "https://github.com/DUNE/dunedetdataformats/archive/refs/tags/v4_4_5.tar.gz"
+    version("4_4_5", url = "https://github.com/DUNE/dunedetdataformats/archive/refs/tags/v4_4_5.tar.gz", sha256="82e69cb0397d910f53664a8276766230489440f37001a4980520b13c7e23b4fc")
     version("4_4_4", url = "https://github.com/DUNE/dunedetdataformats/archive/refs/tags/v4_4_4.tar.gz", sha256="ae4f18a4f3c09f503a0dca5373fb9b08ad04bfc4ead323605121ff3ec76a22df")
     version("4_4_0", url = "https://github.com/DUNE/dunedetdataformats/archive/refs/tags/v4_4_0.tar.gz", sha256="1312f255869f6b021df8c9a7885925192e62094f46808d3b7f6bd99d6efc0a20")
     version("4_1_0", url = "https://github.com/DUNE/dunedetdataformats/archive/refs/tags/v4_1_0.tar.gz", sha256="479de5f1392b6303c258bced663b9aebd22ccd4a0aab2dd2910a9e1e295808b8")
