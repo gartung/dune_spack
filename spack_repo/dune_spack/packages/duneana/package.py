d35d6d282e84d65d13380bd9ebd0812dd40807bd
Patrick Gartung <gartung@fnal.gov>
2025-09-25 10:52:09 -0500

    More typos

--- spack_repo/dune_spack/packages/duneana/package.py
+++ spack_repo/dune_spack/packages/duneana/package.py
@@ -60,9 +60,9 @@ class Duneana(CMakePackage, FnalGithubPackage):
                  )
         filter_file(
                 r"find_package\( larfinder REQUIRED \)",
-                "find_package( larfinder REQUIRED )\nset(CMAKE_FIND_LIBRARY_SUFFIXES ".so", ".so.2")\nfind_package(TensorFlow REQUIRED)",
+                'find_package( larfinder REQUIRED )\nset(CMAKE_FIND_LIBRARY_SUFFIXES ".so", ".so.2")\nfind_package(TensorFlow REQUIRED)',
                 "CMakeLists.txt"
-                )`
+                )
 
     depends_on("c", type="build")
     depends_on("cxx", type="build")
