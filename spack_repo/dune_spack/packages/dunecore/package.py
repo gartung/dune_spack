d9c81d84b262de82d6204ec1a82042002d1cc53c
Patrick Gartung <gartung@fnal.gov>
2026-03-03 17:09:50 -0600

    dunecore can depend on any hdf5@1.12:1.14

--- spack_repo/dune_spack/packages/dunecore/package.py
+++ spack_repo/dune_spack/packages/dunecore/package.py
@@ -74,7 +74,7 @@ class Dunecore(CMakePackage):
     depends_on("gallery")
     depends_on("geant4")
     depends_on("genie")
-    depends_on("hdf5@1.12.2")
+    depends_on("hdf5@1.12:1.14")
     depends_on("hep-concurrency")
     depends_on("highfive@:3")
     depends_on("ifdh-art")
